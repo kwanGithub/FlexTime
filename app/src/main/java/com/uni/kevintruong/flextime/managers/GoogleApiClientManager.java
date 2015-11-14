00d2f74ea2ec7036a45ec2bd150c03ee59d6e91b
Kevin Truong <kwan.github@gmail.com>
2015-11-14 19:46:00 +0100

    new getlocation

--- app/src/main/java/com/uni/kevintruong/flextime/managers/GoogleApiClientManager.java
+++ app/src/main/java/com/uni/kevintruong/flextime/managers/GoogleApiClientManager.java
@@ -1,5 +1,6 @@
 package com.uni.kevintruong.flextime.managers;
 
+import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by kwan8 on 2015-11-14.
@@ -17,10 +19,61 @@ import com.google.android.gms.location.LocationListener;
 public class GoogleApiClientManager implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status>, LocationListener
 {
+
+    private static GoogleApiClientManager instance;
+    private GoogleApiClient googleApiClient;
+    private Location location;
+    private Context context;
+
+    public static synchronized GoogleApiClientManager getGoogleApiClientManager(Context context)
+    {
+        if (instance == null)
+        {
+            instance = new GoogleApiClientManager(context.getApplicationContext());
+        }
+
+        return instance;
+    }
+
+    private GoogleApiClientManager(Context context)
+    {
+        this.context = context;
+        this.googleApiClient = buildGoogleApiClient(this.context);
+        connectGoogleApiClient();
+    }
+
+    private void connectGoogleApiClient()
+    {
+        if(this.googleApiClient == null)
+        {
+            this.googleApiClient.connect();
+        }
+    }
+
+    private GoogleApiClient buildGoogleApiClient(Context context)
+    {
+        return new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    public void geeLocation()
+    {
+
+    }
+
     @Override
     public void onConnected(Bundle bundle)
     {
+        this.location = LocationServices.FusedLocationApi.getLastLocation(this.googleApiClient);
 
+        //DEBUG
+        if(this.location != null)
+        {
+
+        }
     }
 
     @Override
