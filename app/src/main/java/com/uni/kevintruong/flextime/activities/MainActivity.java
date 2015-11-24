0e6f47d3de26933f3746fcb17e7d88be46d1ecf3
Kevin Truong <kwan.github@gmail.com>
2015-11-24 21:37:31 +0100

    Design

--- app/src/main/java/com/uni/kevintruong/flextime/activities/MainActivity.java
+++ app/src/main/java/com/uni/kevintruong/flextime/activities/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         DatabaseManager db;
         db = DatabaseManager.getInstance(this);
         GeofenceManager.getInstance(this, db.mapGeolocationsToGeofences(db.getGeoLocations()));
